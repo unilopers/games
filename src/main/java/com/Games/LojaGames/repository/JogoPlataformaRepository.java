61a2fc6642da9110e4e2396889b696aad13a0adc
DanNikk0 <danielsampaio1708@gmail.com>
2025-11-25 14:19:46 -0300

    :sparkles: feat (repository): JogoPlataformaRepository

---
+++ src/main/java/com/Games/LojaGames/repository/JogoPlataformaRepository.java
@@ -0,0 +1,7 @@
+package com.Games.LojaGames.repository;
+
+import com.grupo_07.games.model.JogoPlataforma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JogoPlataformaRepository extends JpaRepository<JogoPlataforma, Long> {
+}
