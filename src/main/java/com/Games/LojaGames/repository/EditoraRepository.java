7100395e4c1f63c1a31b37718c301cbca61e632e
DanNikk0 <danielsampaio1708@gmail.com>
2025-11-25 14:18:33 -0300

    :sparkles: feat (repository): Editora Repository

---
+++ src/main/java/com/Games/LojaGames/repository/EditoraRepository.java
@@ -0,0 +1,9 @@
+package com.Games.LojaGames.repository;
+
+import com.grupo_07.games.model.Editora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditoraRepository extends JpaRepository<Editora, Long> {
+
+}
+
