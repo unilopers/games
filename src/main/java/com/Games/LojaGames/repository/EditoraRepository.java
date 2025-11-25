bd15a3f9aa74ccf51cd8d4da62b8309d5a2c9403
Malv741 <malvcronos@gmail.com>
2025-11-25 17:52:16 -0300

    :bug: fix(imports): Corrige nome da importação para utilização da pasta correta

--- src/main/java/com/Games/LojaGames/repository/EditoraRepository.java
+++ src/main/java/com/Games/LojaGames/repository/EditoraRepository.java
@@ -1,6 +1,6 @@
 package com.Games.LojaGames.repository;
 
-import com.grupo_07.games.model.Editora;
+import com.Games.LojaGames.model.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
