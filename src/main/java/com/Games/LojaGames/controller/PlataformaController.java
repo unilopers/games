3bd35e764db79270b270febb3dd6e41d2afdf748
Malv741 <malvcronos@gmail.com>
2025-11-25 17:56:59 -0300

    :fire: prune: Remove código comentado

--- src/main/java/com/Games/LojaGames/controller/PlataformaController.java
+++ src/main/java/com/Games/LojaGames/controller/PlataformaController.java
@@ -1,10 +1,10 @@
 package com.Games.LojaGames.controller;
 
-import com.grupo_07.games.model.Plataforma;
-import com.grupo_07.games.repository.PlataformaRepository;
+import com.Games.LojaGames.model.Plataforma;
+import com.Games.LojaGames.repository.PlataformaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType; // Importante
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +18,17 @@ public class PlataformaController {
     @Autowired
     PlataformaRepository plataformaRepository;
 
-    // GET: Produz XML
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     public List<Plataforma> readAll(){
         return plataformaRepository.findAll();
     }
 
-    // GET ID: Produz XML
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<Plataforma> readById(@PathVariable Long id){
         var plataforma = plataformaRepository.findById(id);
         return plataforma.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // POST: Consome e Produz XML
     @PostMapping(
             consumes = MediaType.APPLICATION_XML_VALUE,
             produces = MediaType.APPLICATION_XML_VALUE
@@ -40,7 +37,6 @@ public class PlataformaController {
         var plat = this.plataformaRepository.findByNome(plataforma.getNome());
 
         if (plat != null) {
-            // Retorna um erro formatado em XML simples
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .contentType(MediaType.APPLICATION_XML)
                     .body("<erro>Uma plataforma com esse nome já existe!!</erro>");
@@ -50,7 +46,6 @@ public class PlataformaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newPlataforma);
     }
 
-    // PUT: Consome e Produz XML
     @PutMapping(
             value = "/{id}",
             consumes = MediaType.APPLICATION_XML_VALUE,
