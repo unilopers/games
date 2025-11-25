0e066084c59e49a4f47362d58f7b81473fecc585
Malv741 <malvcronos@gmail.com>
2025-11-25 16:37:40 -0300

    :sparkles: feat(Repository): Adição da Repository Pedido

---
+++ src/main/java/com/Games/LojaGames/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.Games.LojaGames.repository;
+
+import com.Games.LojaGames.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido,Long> {
+}
