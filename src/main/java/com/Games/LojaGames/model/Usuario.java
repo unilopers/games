3bcc5ad7af243a207822cd76bde22b4fd19dfd39
Rafael Ferreira <rafael.fetreira.silva.12345@gmail.com>
2025-11-25 23:19:56 -0300

    :adhesive_bandage: feat(model): Corrigindo model usuario

--- src/main/java/com/Games/LojaGames/model/Usuario.java
+++ src/main/java/com/Games/LojaGames/model/Usuario.java
@@ -1,4 +1,129 @@
 package com.Games.LojaGames.model;
 
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CreationTimestamp; // Importante para a data automática
+
+import java.time.LocalDateTime; // Importante para lidar com TIMESTAMP
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_usuario", nullable = false, unique = true, length = 60)
+    private String nomeUsuario;
+
+    @Column(nullable = false, unique = true, length = 200)
+    private String email;
+
+    @Column(name = "senha_hash", nullable = false, length = 255)
+    private String senha;
+
+    @Column(name = "nome_completo", length = 200) // Ajustei o tamanho conforme imagem (parece ser 200 ou 255)
+    private String nomeCompleto;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "funcao")
+    private int funcao;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", updatable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "ultimo_login")
+    private LocalDateTime ultimoLogin;
+
+    // -------------------------------
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("usuario")
+    private List<Endereco> enderecos;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("usuario")
+    private List<Avaliacao> avaliacoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(int funcao) {
+        this.funcao = funcao;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(LocalDateTime ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+    
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {''
+        this.avaliacoes = avaliacoes;
+    }
 }
