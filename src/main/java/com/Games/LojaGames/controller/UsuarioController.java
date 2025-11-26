425b797c8e3b6c17ef4615b82b7cdbf195d66994
Rafael Ferreira <rafael.fetreira.silva.12345@gmail.com>
2025-11-25 23:39:09 -0300

    :adhesive_bandage: feat(controller): Correção do controller

--- src/main/java/com/Games/LojaGames/controller/UsuarioController.java
+++ src/main/java/com/Games/LojaGames/controller/UsuarioController.java
@@ -1,4 +1,48 @@
 package com.Games.LojaGames.controller;
 
+import com.Games.LojaGames.model.Usuario;
+import com.Games.LojaGames.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuariosRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        return ResponseEntity.ok(usuariosRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        return usuariosRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuariosRepository.save(usuario));
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> put(@RequestBody Usuario usuario) {
+        return usuariosRepository.findById(usuario.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(usuariosRepository.save(usuario)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        usuariosRepository.deleteById(id);
+    }
 }
