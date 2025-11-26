69a5f43aaa71f09c8381283af3e0ce566cbc69c5
Malv741 <malvcronos@gmail.com>
2025-11-25 23:43:07 -0300

    :adhesive_bandage: fix(repository): Completa implementação repository

--- src/main/java/com/Games/LojaGames/repository/ItemListaDesejoRepository.java
+++ src/main/java/com/Games/LojaGames/repository/ItemListaDesejoRepository.java
@@ -1,4 +1,12 @@
 package com.Games.LojaGames.repository;
 
-public interface ItemListaDesejoRepository {
+import com.Games.LojaGames.model.ItemListaDesejo;
+import com.Games.LojaGames.model.ItemListaDesejoId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemListaDesejoRepository extends JpaRepository<ItemListaDesejo, Long> {
+    ItemListaDesejo findById(ItemListaDesejoId id);
 }
+
